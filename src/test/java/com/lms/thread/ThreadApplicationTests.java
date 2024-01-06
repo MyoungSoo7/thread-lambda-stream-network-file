941b72a691deb8f4238558f2df0ba4fa4023e839
iamipro <iamipro@naver.com>
2024-01-07 03:00:40 +0900

    Initial commit

---
+++ src/test/java/com/lms/thread/ThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
